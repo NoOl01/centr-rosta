22aa5435f8a19e4439e5f602dc45e815ec8fa5cd
NoOl01 <tagaevanton0@gmail.com>
2026-02-12 14:08:13 +0200

    feat(session): add delete method

--- internal/infra/session/delete.go
+++ internal/infra/session/delete.go
@@ -1,8 +1,16 @@
 package session
 
-import "context"
+import (
+	"centr_rosta/internal/consts"
+	"centr_rosta/pkg/logger"
+	"context"
+)
 
 func (s *sessionRepository) Delete(ctx context.Context, sessionID string) error {
-	//TODO implement me
-	panic("implement me")
+	if err := s.rdb.Del(ctx, sessionID).Err(); err != nil {
+		logger.Log.Error(consts.RedisSession, err.Error())
+		return err
+	}
+
+	return nil
 }
